f56a627bc3186749578dca3723fe2b39b79709e4
SeungGumon <b9327912@gmail.com>
2021-12-14 22:29:40 +0900

    download recoil

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,25 +1,24 @@
 import styled from "styled-components";
-import {useEffect, useState} from "react";
 import {
-    Switch,
-    Route,
-    useLocation,
-    useParams,
-    useRouteMatch,
+  Switch,
+  Route,
+  useLocation,
+  useParams,
+  useRouteMatch,
 } from "react-router-dom";
 import Price from "./Price";
 import Chart from "./Chart";
-import {Link} from 'react-router-dom'
-import {useQuery} from "react-query";
-import {fetchCoinInfo, fetchCoinTickers} from "../api";
-import {Helmet} from 'react-helmet';
+import { Link } from "react-router-dom";
+import { useQuery } from "react-query";
+import { fetchCoinInfo, fetchCoinTickers } from "../api";
+import { Helmet } from "react-helmet";
 
 const Header = styled.header`
   height: 15vh;
   display: flex;
   justify-content: center;
   align-items: center;
-`
+`;
 const Tabs = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
@@ -36,7 +35,7 @@ const Tab = styled.span<{ isActive: boolean }>`
   padding: 7px 0px;
   border-radius: 10px;
   color: ${(props) =>
-          props.isActive ? props.theme.accentColor : props.theme.textColor};
+    props.isActive ? props.theme.accentColor : props.theme.textColor};
 
   a {
     display: block;
@@ -69,11 +68,10 @@ const Wrapper = styled.div`
   max-width: 950px;
   width: 100%;
   margin: 0 auto;
-`
-
+`;
 
 const Description = styled.p`
-  margin: 20px 0px;
+  margin: 20px 0;
 `;
 
 const Container = styled.div`
@@ -82,162 +80,168 @@ const Container = styled.div`
 
 const Title = styled.h1`
   font-size: 48px;
-  color: ${props => props.theme.accentColor}
-`
+  color: ${(props) => props.theme.accentColor};
+`;
 
 const Loader = styled.span`
   text-align: center;
   display: block;
-`
+`;
 
 export interface IParams {
-    coinId: string
+  coinId: string;
 }
 
 interface RouteState {
-    name: string
+  name: string;
 }
 
-
 interface InfoData {
-    id: string;
-    name: string;
-    symbol: string;
-    rank: number;
-    is_new: boolean;
-    is_active: boolean;
-    type: string;
-    description: string;
-    message: string;
-    open_source: boolean;
-    started_at: string;
-    development_status: string;
-    hardware_wallet: boolean;
-    proof_type: string;
-    org_structure: string;
-    hash_algorithm: string;
-    first_data_at: string;
-    last_data_at: string;
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  first_data_at: string;
+  last_data_at: string;
 }
 
 interface PriceData {
-    id: string;
-    name: string;
-    symbol: string;
-    rank: number;
-    circulating_supply: number;
-    total_supply: number;
-    max_supply: number;
-    beta_value: number;
-    first_data_at: string;
-    last_updated: string;
-    quotes: {
-        USD: {
-            ath_date: string;
-            ath_price: number;
-            market_cap: number;
-            market_cap_change_24h: number;
-            percent_change_1h: number;
-            percent_change_1y: number;
-            percent_change_6h: number;
-            percent_change_7d: number;
-            percent_change_12h: number;
-            percent_change_15m: number;
-            percent_change_24h: number;
-            percent_change_30d: number;
-            percent_change_30m: number;
-            percent_from_price_ath: number;
-            price: number;
-            volume_24h: number;
-            volume_24h_change_24h: number;
-        };
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: {
+      ath_date: string;
+      ath_price: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_1h: number;
+      percent_change_1y: number;
+      percent_change_6h: number;
+      percent_change_7d: number;
+      percent_change_12h: number;
+      percent_change_15m: number;
+      percent_change_24h: number;
+      percent_change_30d: number;
+      percent_change_30m: number;
+      percent_from_price_ath: number;
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
     };
+  };
 }
 
-
 function Coin() {
+  const { coinId } = useParams<IParams>();
+  const { state } = useLocation<RouteState>();
+  const priceMatch = useRouteMatch("/:coinId/price");
+  const chartMatch = useRouteMatch("/:coinId/chart");
 
-    const {coinId} = useParams<IParams>();
-    const {state} = useLocation<RouteState>();
-    const priceMatch = useRouteMatch("/:coinId/price");
-    const chartMatch = useRouteMatch("/:coinId/chart");
+  const { isLoading: coinInfoLoading, data: coinInfoData } = useQuery<InfoData>(
+    ["info", coinId],
+    () => fetchCoinInfo(coinId)
+  );
+  const { isLoading: coinPriceLoading, data: coinPrice } = useQuery<PriceData>(
+    ["tickers", coinId],
+    () => fetchCoinTickers(coinId),
+    {
+      refetchInterval: 5000,
+    }
+  );
 
+  const loading = coinInfoLoading || coinPriceLoading;
 
-    const {
-        isLoading: coinInfoLoading,
-        data: coinInfoData
-    } = useQuery<InfoData>(["info", coinId], () => fetchCoinInfo(coinId));
-    const {
-        isLoading: coinPriceLoading,
-        data: coinPrice
-    } = useQuery<PriceData>(["tickers", coinId], () => fetchCoinTickers(coinId), {
-        refetchInterval : 5000
-    })
+  return (
+    <Container>
+      <Helmet>
+        <title>
+          {state?.name
+            ? state.name
+            : loading
+            ? "Loading..."
+            : coinInfoData?.name}
+        </title>
+      </Helmet>
+      <Header>
+        <Title>
+          {state?.name
+            ? state.name
+            : loading
+            ? "Loading..."
+            : coinInfoData?.name}
+        </Title>
+      </Header>
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <Wrapper>
+          <Overview>
+            <OverviewItem>
+              <span>Rank:</span>
+              <span>{coinInfoData?.rank}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Symbol:</span>
+              <span>${coinInfoData?.symbol}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Price:</span>
+              <span>{coinPrice?.quotes.USD.price.toFixed(3)}</span>
+            </OverviewItem>
+          </Overview>
+          <Description>{coinInfoData?.description}</Description>
+          <Overview>
+            <OverviewItem>
+              <span>Total Suply:</span>
+              <span>{coinPrice?.total_supply}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Max Supply:</span>
+              <span>{coinPrice?.max_supply}</span>
+            </OverviewItem>
+          </Overview>
 
+          <Tabs>
+            <Tab isActive={chartMatch !== null}>
+              <Link to={`/${coinId}/chart`}>Chart</Link>
+            </Tab>
+            <Tab isActive={priceMatch !== null}>
+              <Link to={`/${coinId}/price`}>Price</Link>
+            </Tab>
+          </Tabs>
 
-    const loading = coinInfoLoading || coinPriceLoading;
-
-    return <Container>
-        <Helmet>
-            <title>{state?.name ? state.name : loading ? "Loading..." : coinInfoData?.name}</title>
-        </Helmet>
-        <Header>
-            <Title>
-                {state?.name ? state.name : loading ? "Loading..." : coinInfoData?.name}
-            </Title>
-        </Header>
-        {loading ? (
-            <Loader>Loading...</Loader>
-        ) : (
-            <Wrapper>
-                <Overview>
-                    <OverviewItem>
-                        <span>Rank:</span>
-                        <span>{coinInfoData?.rank}</span>
-                    </OverviewItem>
-                    <OverviewItem>
-                        <span>Symbol:</span>
-                        <span>${coinInfoData?.symbol}</span>
-                    </OverviewItem>
-                    <OverviewItem>
-                        <span>Price:</span>
-                        <span>{coinPrice?.quotes.USD.price.toFixed(3)}</span>
-                    </OverviewItem>
-                </Overview>
-                <Description>{coinInfoData?.description}</Description>
-                <Overview>
-                    <OverviewItem>
-                        <span>Total Suply:</span>
-                        <span>{coinPrice?.total_supply}</span>
-                    </OverviewItem>
-                    <OverviewItem>
-                        <span>Max Supply:</span>
-                        <span>{coinPrice?.max_supply}</span>
-                    </OverviewItem>
-                </Overview>
-
-                <Tabs>
-                    <Tab isActive={chartMatch !== null}>
-                        <Link to={`/${coinId}/chart`}>Chart</Link>
-                    </Tab>
-                    <Tab isActive={priceMatch !== null}>
-                        <Link to={`/${coinId}/price`}>Price</Link>
-                    </Tab>
-                </Tabs>
-
-                <Switch>
-                    <Route path={`/${coinId}/price`}>
-                        <Price/>
-                    </Route>
-                    <Route path={`/${coinId}/chart`}>
-                        <Chart
-                            coinId={coinId}
-                        />
-                    </Route>
-                </Switch>
-            </Wrapper>
-        )}
+          <Switch>
+            <Route path={`/${coinId}/price`}>
+              <Price />
+            </Route>
+            <Route path={`/${coinId}/chart`}>
+              <Chart coinId={coinId} />
+            </Route>
+          </Switch>
+        </Wrapper>
+      )}
     </Container>
+  );
 }
 
 export default Coin;
