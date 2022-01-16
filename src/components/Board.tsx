5df03687bdb94dc0ea98ba81ee5e7c62cbc41d49
SeungGumon <b9327912@gmail.com>
2022-01-17 02:00:41 +0900

    cross movement 6-10

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,36 +1,55 @@
-import DragabbleCard from "./DragabbleCard";
-import {Droppable} from "react-beautiful-dnd";
-import React from "react";
+import { Droppable } from "react-beautiful-dnd";
 import styled from "styled-components";
-
+import DragabbleCard from "./DragabbleCard";
 
 const Wrapper = styled.div`
+  width: 300px;
   padding: 20px 10px;
-  padding-top: 30px;
+  padding-top: 10px;
   background-color: ${(props) => props.theme.boardColor};
   border-radius: 5px;
-  min-height: 200px;
+  min-height: 300px;
+  display: flex;
+  flex-direction: column;
 `;
+
+const Title = styled.h2`
+  text-align: center;
+  font-weight: 600;
+  margin-bottom: 10px;
+  font-size: 18px;
+`;
+
+
+const Area = styled.div`
+  flex-grow: 1;
+`
+
 
 
 interface IBoardProps {
     toDos: string[];
-    boardId: string
+    boardId: string;
 }
 
-function Board({toDos, boardId}: IBoardProps) {
+function Board({ toDos, boardId }: IBoardProps) {
     return (
-        <Droppable droppableId={boardId}>
-            {(magic) => (
-                <Wrapper ref={magic.innerRef} {...magic.droppableProps}>
-                    {toDos.map((toDo, index) => (
-                        <DragabbleCard key={toDo} index={index} toDo={toDo}/>
-                    ))}
-                    {magic.placeholder}
-                </Wrapper>
-            )}
-        </Droppable>
-    )
+        <Wrapper>
+            <Title>{boardId}</Title>
+            <Droppable droppableId={boardId}>
+                {(magic) => (
+                    <Area
+                        ref={magic.innerRef}
+                        {...magic.droppableProps}
+                    >
+                        {toDos.map((toDo, index) => (
+                            <DragabbleCard key={toDo} index={index} toDo={toDo} />
+                        ))}
+                        {magic.placeholder}
+                    </Area>
+                )}
+            </Droppable>
+        </Wrapper>
+    );
 }
-
 export default Board;
--- src/routes/Drag.tsx
+++ src/routes/Drag.tsx
@@ -29,14 +29,36 @@ const Boards = styled.div`
 
 function Drag() {
     const [toDos, setToDos] = useRecoilState(toDoStateTwo);
-    const onDragEnd = ({ draggableId, destination, source }: DropResult) => {
+    const onDragEnd = (info: DropResult) => {
+        console.log(info);
+        const { destination, draggableId, source } = info;
         if (!destination) return;
-        // setToDos((oldToDos) => {
-        //     const toDosCopy = [...oldToDos];
-        //     toDosCopy.splice(source.index, 1);
-        //     toDosCopy.splice(destination?.index, 0, draggableId);
-        //     return toDosCopy;
-        // });
+        if (destination?.droppableId === source.droppableId) {
+            // same board movement.
+            setToDos((allBoards) => {
+                const boardCopy = [...allBoards[source.droppableId]];
+                boardCopy.splice(source.index, 1);
+                boardCopy.splice(destination?.index, 0, draggableId);
+                return {
+                    ...allBoards,
+                    [source.droppableId]: boardCopy,
+                };
+            });
+        }
+        if (destination.droppableId !== source.droppableId) {
+            // cross board movement
+            setToDos((allBoards) => {
+                const sourceBoard = [...allBoards[source.droppableId]];
+                const destinationBoard = [...allBoards[destination.droppableId]];
+                sourceBoard.splice(source.index, 1);
+                destinationBoard.splice(destination?.index, 0, draggableId);
+                return {
+                    ...allBoards,
+                    [source.droppableId]: sourceBoard,
+                    [destination.droppableId]: destinationBoard,
+                };
+            });
+        }
     };
     return (
         <DragDropContext onDragEnd={onDragEnd}>
